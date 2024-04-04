fd27fec54ef52be059035bd8d7b9149a4544a1a1
IvanBP-UMA <ivanbezaresp@uma.es>
2024-04-04 17:10:04 +0200

    Changed sondeo2.s so that GUAC agrees with me

--- sondeo2.s
+++ sondeo2.s
@@ -19,7 +19,7 @@ ledseq:
 	push {r4}
 	mov r2, #0
 	mov r4, #0
-	loop:
+	mainLoop:
 		cmp r2, r1
 		subeq r2, r2, r1
 		ldr r3, [r0, r2, LSL #2]
@@ -31,35 +31,35 @@ ledseq:
 		pop {lr}
 		pop {r0-r3}
 		
-		push {r0-r3}
-		push {lr}
-		mov r0, #0
-		bl isButtonPressed
-		cmp r0, r4
-		pop {lr}
-		pop {r0-r3}
-		bgt isPressed
-		blt isReleased
-		b continue
+		buttonLoop:
+			push {r0-r3}
+			push {lr}
+			mov r0, #3
+			bl isButtonPressed
+			cmp r0, #0
+			pop {lr}
+			pop {r0-r3}
+			bne exit
 		
-		isPressed: add r4, #1
-					b continue
-		isReleased: add r2, #1
+			push {r0-r3}
+			push {lr}
+			mov r0, #0
+			bl isButtonPressed
+			cmp r0, r4
+			pop {lr}
+			pop {r0-r3}
+			bgt isPressed
+			blt isReleased
+			b buttonLoop
+			isPressed: add r4, #1
+					b buttonLoop
+			isReleased: add r2, #1
 					sub r4, #1
-					b continue
+					b mainLoop
 	
-		continue:
-		push {r0-r3}
-		push {lr}
-		mov r0, #3
-		bl isButtonPressed
-		cmp r0, #0
-		pop {lr}
-		pop {r0-r3}
-		bne exit
-		b loop
-		
+			
 	exit:
 		pop {r4}
 		bx lr
-		
+	
+	
